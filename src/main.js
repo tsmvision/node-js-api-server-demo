023760e2de825bbf0dab247ba806cba7c12f150e
Luke <lukelee01@gmail.com>
2019-03-23 14:39:41 -0400

    feature: setup bodyParser for dummy POST.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import {homeRoute} from './routes/routes';
+import bodyParser from 'body-parser';
+import {homeRoute, registerUserRoute} from './routes/routes';
 
 const app = express();
 
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// application/json
+app.use(jsonParser);
 app.use(homeRoute);
+app.use(registerUserRoute);
 
 const portNumber = 8000;
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 const homeRoute = router.get("/", (req, res) => {
-  res.send({
+  res.status(200).send({
     message: "Hello World!"
   });
 });
 
+const registerUserRoute = router.post("/register-user", (req, res) => {
+  const id = req.body.id;
+  const password = req.body.password;
+
+  console.log("req.body: ", req.body);
+
+  res.status(201).json({
+    message: "user regisistered successfully",
+    data: {
+      id,
+      password
+    }
+  });
+});
+
 export {
-  homeRoute
+  homeRoute,
+  registerUserRoute
 }
