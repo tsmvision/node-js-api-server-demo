f54a236658516c2eb4d39b58adb3b93bab076701
Luke <lukelee01@gmail.com>
2019-03-23 19:27:41 -0400

    refactor: remove unused codes in the homeController.js.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,7 @@
 const homeController = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      errors: errors.array()
-    });
-  }
-
   res.status(200).send({
     message: "Hello World!"
   });
 };
-
-
 
 export {homeController};
