35d1f6baad03af80fb0f2383bfbbcf7f39821389
Luke <lukelee01@gmail.com>
2019-03-23 17:24:52 -0400

    feature: add Jest and create dummy test.test.js file.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import {homeController, registerUserController} from '../controllers'
 
-
 const router = express.Router();
 
 const homeRoute = router.get("/", homeController);
-
 const registerUserRoute = router.post("/register-user", registerUserController);
 
 export {
