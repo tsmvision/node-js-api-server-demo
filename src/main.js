5b45f223b2e7f00fc109fc10b59f30fcd25240e6
Luke <lukelee01@gmail.com>
2019-03-23 14:02:44 -0400

    refactor: remove unused codes in the main.js.

--- src/main.js
+++ src/main.js
@@ -5,18 +5,8 @@ const app = express();
 
 app.use(homeRoute);
 
-// app.get("/", (req, res) => {
-//   res.send({
-//     message: "Hello World!"
-//   });
-// });
-
 const portNumber = 8000;
 
 app.listen(portNumber, () => {
   console.log(`Server running on port ${portNumber}`);
 });
-
-
-// ESM syntax is supported.
-// export {}
