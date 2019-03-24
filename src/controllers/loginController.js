40369d8019c2c832ba08d71ac49542983da2aeb9
Luke <lukelee01@gmail.com>
2019-03-24 00:00:41 -0400

    refactor: add jwt creating code with dummy static data in loginController.js.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,19 @@
+import {tokenGenerator} from '../common'
+
+
 const loginController = (req, res) => {
-  res.status(200).send({
-    message: "Hello World!"
+  const { email, password } = req.body;
+
+  //TODO: check if email exists in the db
+  if (email === "lukelee111@gmail.com" && password === "password1") {
+    res.status(200).send({
+      message: `${email} login successfully`,
+      token: tokenGenerator({email})
+    });
+  }
+
+  res.status(400).send({
+    message: "login failed"
   });
 };
 
