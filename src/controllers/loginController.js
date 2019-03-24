ab4e449eb1ecf4f667a00b915094519e28b47699
Luke <lukelee01@gmail.com>
2019-03-24 13:06:32 -0400

    refactor: apply Sequelize in loginController.js.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,19 +1,30 @@
 import {tokenGenerator} from '../common'
+import {User} from '../models';
 
 const loginController = (req, res) => {
   const {email, password} = req.body;
 
-  //TODO: check if email exists in the db
-  if (email === "lukelee111@gmail.com" && password === "password1") {
-    return res.status(200).send({
-      message: `${email} was logged in successfully`,
-      token: tokenGenerator({email})
-    });
-  }
+  User.findOne({
+    where: {
+      email: email,
+      password: password
+    }
+  }).then(
+    (user) => {
+      // check if the user exists.
+      if (user) {
+        return res.status(200).send({
+          message: `${email} was logged in successfully`,
+          token: tokenGenerator({email})
+        });
+      }
 
-  return res.status(400).send({
-    message: "login failed"
-  });
+      return res.status(400).send({
+        message: "login failed",
+        token: ""
+      });
+    }
+  );
 };
 
 export {
