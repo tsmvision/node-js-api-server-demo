784c9452b712fc96683f01a4ad5431d9419a6715
Luke <lukelee01@gmail.com>
2019-03-24 00:15:34 -0400

    fix: fix header error by modify loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,17 +2,17 @@ import {tokenGenerator} from '../common'
 
 
 const loginController = (req, res) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
   //TODO: check if email exists in the db
   if (email === "lukelee111@gmail.com" && password === "password1") {
-    res.status(200).send({
+    return res.status(200).send({
       message: `${email} login successfully`,
       token: tokenGenerator({email})
     });
   }
 
-  res.status(400).send({
+  return res.status(400).send({
     message: "login failed"
   });
 };
